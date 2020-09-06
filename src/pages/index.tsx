0feeda72bfd750f14bd2353b92d279e1d9e47026
Sergey Belash <again.against@gmail.com>
2020-09-06 11:20:02 +0200

    head/title added for better browser tab na,e

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import { NavBar } from "../components/NavBar";
 
 const Index = () => (
   <>
+    <Head>
+      <title>Lireddit</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <NavBar />
-    <div>hello world</div>;
+    <div>hello world</div>
   </>
 );
 
