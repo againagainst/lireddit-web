11c4f1647d16479d501bc8e2cd7677b895ecc615
Sergey Belash <again.against@gmail.com>
2020-09-27 11:42:18 +0200

    Posts page styling and text eliding

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -40,6 +40,7 @@ export type Post = {
   creatorId: Scalars['Float'];
   createdAt: Scalars['String'];
   updatedAt: Scalars['String'];
+  textSnippet: Scalars['String'];
 };
 
 export type User = {
@@ -245,7 +246,7 @@ export type PostsQuery = (
   { __typename?: 'Query' }
   & { posts: Array<(
     { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'title' | 'createdAt' | 'updatedAt'>
+    & Pick<Post, 'id' | 'title' | 'text' | 'textSnippet' | 'createdAt' | 'updatedAt'>
   )> }
 );
 
@@ -362,6 +363,8 @@ export const PostsDocument = gql`
   posts(limit: $limit, cursor: $cursor) {
     id
     title
+    text
+    textSnippet
     createdAt
     updatedAt
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,50 @@
-import { Link } from "@chakra-ui/core";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
+import React from "react";
 import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10,
     },
   });
+
+  if (!fetching && !data) {
+    return <div> Error loading data...</div>;
+  }
+
   return (
     <Layout>
-      <NextLink href="/create-post ">
-        <Link>create post</Link>
-      </NextLink>
+      <Flex>
+        <Heading>Lireddit</Heading>
+        <NextLink href="/create-post ">
+          <Link ml="auto">create post</Link>
+        </NextLink>
+      </Flex>
       <br />
-      {!data ? (
+      {fetching && !data ? (
         <div>loading...</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={8}>
+          {data!.posts.map((p) => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.text}</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
+      {data ? (
+        <Flex>
+          <Button isLoading={fetching} m="auto" my={8}>
+            Load More
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
