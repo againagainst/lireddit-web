6244dd940b23a8700a9d144fab7d3ad97e8db370
Sergey Belash <again.against@gmail.com>
2020-10-22 19:44:05 +0200

    Move delete and edit button into the posts details.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,7 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  IconButton,
-  Link,
-  Stack,
-  Text,
-} from "@chakra-ui/core";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
 import { Layout } from "components/Layout";
 import { UpdootSection } from "components/UpdootSection";
-import { useDeletePostMutation, usePostsQuery } from "generated/graphql";
+import { usePostsQuery } from "generated/graphql";
 import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
 import React, { useState } from "react";
@@ -22,8 +13,6 @@ const Index = () => {
     cursor: null as null | string,
   });
   const [{ data, fetching }] = usePostsQuery({ variables });
-
-  const [, deletePost] = useDeletePostMutation();
 
   if (!fetching && !data) {
     return <div> Error loading data...</div>;
@@ -40,24 +29,14 @@ const Index = () => {
               <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
                 <UpdootSection post={p} />
                 <Box flex={1}>
-                  <Flex>
-                    <Box>
-                      <NextLink href="/post/[id]" as={`/post/${p.id}`}>
-                        <Link>
-                          <Heading fontSize="xl">{p.title}</Heading>
-                        </Link>
-                      </NextLink>
-                      <Text fontSize="xs">posted by {p.creator.username}</Text>
-                    </Box>
-                    <IconButton
-                      ml="auto"
-                      icon="delete"
-                      aria-label="Delete Post"
-                      onClick={() => {
-                        deletePost({ id: p.id });
-                      }}
-                    />
-                  </Flex>
+                  <Box>
+                    <NextLink href="/post/[id]" as={`/post/${p.id}`}>
+                      <Link>
+                        <Heading fontSize="xl">{p.title}</Heading>
+                      </Link>
+                    </NextLink>
+                    <Text fontSize="xs">posted by {p.creator.username}</Text>
+                  </Box>
                   <Text mt={4}>{p.textSnippet}</Text>
                 </Box>
               </Flex>
--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,9 +1,10 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/core";
+import { Box, Flex, Heading, IconButton, Link, Text } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
+import NextLink from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import { Layout } from "../../components/Layout";
-import { usePostQuery } from "../../generated/graphql";
+import { useDeletePostMutation, usePostQuery } from "../../generated/graphql";
 import { createUrqlClient } from "../../utils/createUrqlClient";
 
 export const Post = ({}) => {
@@ -14,6 +15,8 @@ export const Post = ({}) => {
     pause: postId === -1,
     variables: { id: postId },
   });
+
+  const [, deletePost] = useDeletePostMutation();
 
   if (fetching) {
     return <Layout>Fetching...</Layout>;
@@ -30,9 +33,27 @@ export const Post = ({}) => {
   return (
     <Layout>
       <Flex p={5} shadow="md" borderWidth="1px">
-        <Box>
-          <Heading fontSize="xl">{data.post.title}</Heading>
-          <Text fontSize="xs">posted by {data.post.creator.username}</Text>
+        <Box flex={1}>
+          <Flex>
+            <Box>
+              <Heading fontSize="xl">{data.post.title}</Heading>
+              <Text fontSize="xs">posted by {data.post.creator.username}</Text>
+            </Box>
+            <Box ml="auto">
+              <NextLink href="/post/edit/[id]" as={`/post/edit/${postId}`}>
+                <IconButton as={Link} icon="edit" aria-label="Edit Post" />
+              </NextLink>
+              <IconButton
+                ml={2}
+                icon="delete"
+                aria-label="Delete Post"
+                onClick={async () => {
+                  await deletePost({ id: postId });
+                  router.push("/");
+                }}
+              />
+            </Box>
+          </Flex>
           <Text mt={4}>{data.post.text}</Text>
         </Box>
       </Flex>
